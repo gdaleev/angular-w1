453a15c47c619aebe31ce50b9201391917aa872e
Georgi Daleev <gdaleev2346@gmail.com>
2024-04-15 16:25:43 +0300

    Added functionality for hiding and showning info

--- hello-world-2101681039/src/app/app.component.ts
+++ hello-world-2101681039/src/app/app.component.ts
@@ -1,13 +1,41 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
+
 export class AppComponent {
-  title = 'hello-world-2101681039';
+  activeSection: string | null = null;
+
+  toggleShow(section: string) {
+    this.activeSection = this.activeSection === section ? null : section;
+  }
+
+  isSectionActive(section: string): boolean {
+    return this.activeSection === section;
+  }
 }
+
+
+
+
+
+
+
+// export class AppComponent {
+//   activeSection: string | null = null;
+
+//   toggleShow(section: string) {
+//     this.activeSection = this.activeSection === section ? null : section;
+//   }
+
+//   isSectionActive(section: string): boolean {
+//     return this.activeSection === section;
+//   }
+// }
